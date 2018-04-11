99af1a70308f8ed8fb8a38a845a35b6c896ae29a
lctavares14 <lctavares14@gmail.com>
2018-04-11 19:21:02 -0400

    nova clas menuproduto

--- src/modelo/Produto.java
+++ src/modelo/Produto.java
@@ -2,46 +2,43 @@ package modelo;
 
 public class Produto {
 
-private String name;
-private int quant;
-private float valor;
-private float resultado;
-
-public Produto (String name, int quant, float valor) {
-	this.name = name;
-	this.quant = quant;
-	this.valor = valor;
-}
-
-
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public int getQuant() {
-	return quant;
-}
-public void setQuant(int quant) {
-	this.quant = quant;
-	this.resultado = quant*valor;
-}
-public float getValor() {
-	return valor*quant;
-}
-public void setValor(float valor) {
-	this.valor = valor;
-	this.resultado = quant*valor;
-}
-public float getResultado() {
-	return resultado;
-}
-public void setResultado(float resultado) {
-	this.resultado = resultado;
-}
-
-
-
-
+	private String name;
+	private Integer quant;
+	private Float valor;
+	private Float valorTotal;
+	
+	public Produto (String name, int quant, float valor) {
+		this.name = name;
+		this.quant = quant;
+		this.valor = valor;
+		this.valorTotal = valor * quant;
+	}
+	
+	public String toString() {
+		return "Nome: " + name + "; Quant.: " + quant + "; Preço: "
+				+ valor + "; Valor Total: " + valorTotal;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getQuant() {
+		return quant;
+	}
+	public void setQuant(int quant) {
+		this.quant = quant;
+		this.valorTotal = quant*valor;
+	}
+	public Float getValor() {
+		return valor*quant;
+	}
+	public void setValor(Float valor) {
+		this.valor = valor;
+		this.valorTotal = quant*valor;
+	}
+	public Float getvalorTotal() {
+		return valorTotal;
+	}	
 }
